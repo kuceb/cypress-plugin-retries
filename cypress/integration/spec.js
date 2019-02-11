f57b88e59a44e4c48e5922231aa6047835f7a070
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-02-11 17:11:13 -0500

    mostly works

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,95 +1,141 @@
 /// <reference types="cypress"/>
 
-window.foo = 0
-window.foo2 = 0
-
 Cypress.config('defaultCommandTimeout', 100)
+const retryNum = 5
+Cypress.env('RETRIES', retryNum)
 
-describe('before hook', ()=>{
-  before(()=>{
-    window.beforeHook = window.beforeHook++ || 0
-    if (window.beforeHook < 1) {
-      // cy.get('fail')
-      expect(false).ok
-    }
-  })
-  it('fail once, skip rest', ()=>{
 
-  })
-  it('all skipped', ()=>{
+
+it('can log', ()=>{
+  cy.log('fooo').then(()=>{
+    Cypress.log({
+      name: 'FOOO'
+    })
 
   })
 })
 
-describe('page', function() {
-  // this.retries(10)
 
-  beforeEach(function(){
-    cy.wrap(true).as('outer')
-    cy.timeout(200)
-    cy.log(window.foo)
-    window.foo++
-    // expect(false).ok
-    if (window.foo > 1 && window.foo <= 3) {
-      cy.get('foo')
-    }
+describe.only('0', () => {
+  const pushHook = name => {
+    console.log(`%c${name}`, 'color:blue')
+    expect(name).ok
+    hooks.push(name)
+  }
+  const hooks = []
+  let foo = 0
+  Cypress.on('fail', (err)=>{
+    hooks.push('FAIL')
+    // debugger
+    throw err
   })
 
-  describe('inner tests', ()=>{
-    beforeEach(()=>{
-      cy.timeout(200)
-      window.innerFoo = (window.innerFoo || 0) + 1
-      if (window.innerFoo < 2) {
-        cy.get('bar')
-      }
+  beforeEach(() => {
+    pushHook('BE 0')
+  })
+  describe('1', () => {
+    beforeEach(() => {
+      pushHook('BE 1')
+      expect(++foo).gt(1)
     })
-    it('inner before each fail once', ()=>{
-      expect(true).ok
+
+    describe('2', () => {
+      beforeEach(() => {
+        pushHook('BE 2')
+        expect(foo).gt(2)
+      })
+      beforeEach(() => {
+        pushHook('BE 2 B')
+        // expect(false).ok
+      })
+      beforeEach(() => {
+        pushHook('BE 2 C')
+        // expect(false).ok
+      })
+      it('test', () => {
+        pushHook('T 2')
+        expect('test body').ok
+        expect(foo).gt(3)
+      })
+      afterEach(function() {
+        // debugger
+        // expect(foo).gt(4)
+        pushHook('AE 2')
+      })
     })
-  })
-
-  it('pass', ()=>{
-    cy.get('bar').should('not.exist')
-  })
-
-  it('fail beforeEach twice', ()=>{
-    expect(true).ok
-  })
-
-  it('fail once', ()=>{
-    cy.log('foo2', window.foo2)
-    expect(++window.foo2).eq(2)
-    window.foo2 = 0
-  })
-
-  it('fail twice', ()=>{
-    cy.log('foo2', window.foo2)
-    expect(++window.foo2).eq(3)
-    window.foo2 = 0
-  })
-
-  it('fail four times', ()=>{
-    cy.log('foo2', window.foo2)
-    expect(++window.foo2).eq(5)
-  })
-
-  describe('scope', ()=>{
-    beforeEach(function () {
+    afterEach(function() {
+      pushHook('AE 1')
       // expect(false).ok
-      cy.spy().as('be_foobar')
-      // cy.log('adsfasdf')
-      // debugger
-      // cy.wrap(false).as('outer')
-      expect(this.outer).to.eq(true)
     })
-    it('can alias', function(){
-      cy.spy().as('foobar')
-      expect(this.foobar).to.not.be.called
-      expect(this.be_foobar).to.not.be.called
-  
-  
+    afterEach(function() {
+      pushHook('AE 1 B')
     })
   })
-
-
+  afterEach(() => {
+    pushHook('AE 0')
+  })
+  after(() => {
+    pushHook('AA 0')
+    console.log('after', hooks)
+    const arr = [
+      'BE 1',
+      'BE 2',
+      'BE 2 B',
+      'T 2',
+      'AE 2',
+      'AE 1',
+      'AE 1 B',
+      'AE 0'
+    ]
+    expect(hooks).eq([
+      "BE 0",
+      "BE 1",
+      "BE 0",
+      "BE 1",
+      "BE 2",
+      "BE 0",
+      "BE 1",
+      "BE 2",
+      "BE 2 B",
+      "BE 2 C",
+      "T 2",
+      "AE 2",
+      "AE 1",
+      "AE 1 B",
+      "AE 0",
+      "BE 0",
+      "BE 1",
+      "BE 2",
+      "BE 2 B",
+      "BE 2 C",
+      "T 2",
+      "AE 2",
+      "AE 1",
+      "AE 1 B",
+      "AE 0",
+      "AA 0"
+    ])
+    expect(hooks).deep.eq(repeat(arr, 3))
+    // expect('afterAll').not.ok
+  })
 })
+
+
+
+describe('retry test', function() {
+  Cypress.env('RETRIES', 4)
+  // this.retries(3)
+  let foo = 0
+  beforeEach(() => {
+    foo++
+    expect(foo).gt(2)
+  })
+  it('will fail 3 times', function() {
+    expect(foo).gt(4)
+  })
+  it('will pass', () => {
+    expect(foo).gt(4)
+  })
+})
+
+const repeat = (arr, i) => Array(i).fill(arr).reduce((a,b)=>a.concat(b))
--- src/index.js
+++ src/index.js
@@ -1,132 +1,151 @@
-if (Cypress.env('RETRIES')) {
-  const MAX_RETRIES = +Cypress.env('RETRIES')
+let _current
+let _chainerId
 
-  const normalFail = err => {
-    throw err
+const debug = function () {
+  console.log.apply(this, arguments)
+  // console.log(Cypress._.cloneDeep(window.state.hooks.slice(-1)[0]))
+}
+
+const getDefaultRetries = () => {
+  return Cypress.env('RETRIES')
+}
+
+const _clone = Cypress.mocha._mocha.Mocha.Test.prototype.clone
+Cypress.mocha._mocha.Mocha.Test.prototype.clone = function() {
+  const ret = _clone.apply(this, arguments)
+  if (this.trueFn) {
+    ret.fn = this.trueFn
+    // this.trueFn = null
+  }
+  ret.id = this.id
+  debug('clone test')
+
+  return ret
+}
+
+const _onRunnableRun = Cypress.runner.onRunnableRun
+Cypress.runner.onRunnableRun = function(runnableRun, runnable, args) {
+  debug('_onRunnableRun')
+
+  const r = runnable
+  const isHook = r.type === 'hook'
+  const isAfterHook = isHook && r.hookName.match(/after/)
+  const isAfterAllHook = isHook && r.hookName.match(/after all/)
+  const isBeforeHook = isHook && r.hookName.match(/before/)
+  const test = r.ctx.currentTest || r
+
+  const logRetry = () => {
+    let shouldLog = true
+    test.ctx.hooksLogged = test.ctx.hooksLogged || {}
+
+    if (isHook) {
+      shouldLog = !test.ctx.hooksLogged[r.hookName]
+      test.ctx.hooksLogged[r.hookName] = true
+    }
+
+    let retryNum = test._currentRetry+1
+
+    if (shouldLog) {
+      // wrap this in a 'try' because a bug in cypress will throw if
+      // you call Cypres.log before any commands in a test
+      try{
+        Cypress.log({
+          name: `Retry ${retryNum}`,
+          type: 'parent',
+          message: '',
+          state: 'retry'
+        })
+      } catch(e){
+        console.log('ERROR')
+      }
+  }
   }
 
-  let failHandler = normalFail
-
-  const removeSkipFail = err => {
-    requestAnimationFrame(() => {
-      try {
-        const el = [].slice
-        .call(
-          window.top.document.querySelectorAll('.test.runnable.runnable-passed')
-        )
-        .slice(-1)[0].nextSibling
-        el.remove()
-      } catch(e){}
-    })
-    throw err
+  if (test._currentRetry > -1) {
+    logRetry()
   }
 
 
-  let currentRetry = 0
-  let shouldSkipTest = false
-  let shouldSkipBefore = false
+  const next = args[0]
 
-  const _it = window.it
-  const _beforeEach = window.beforeEach
-  window.beforeEach = function(...args) {
-    const beforeEachFn = args[0]
-    args[0] = function() {
-      if (shouldSkipBefore) return
-      if (shouldSkipTest) return
 
-      failHandler = err => {
-        if (currentRetry === MAX_RETRIES) {
-          currentRetry = 0
+
+
+  debug('on:', r.title)
+
+  if (r.ctx.currentTest && r.ctx.currentTest.trueFn && !isAfterAllHook) {
+    debug('already failed, skipping this hook')
+    return next.call(this)
+  }
+  debug('running')
+
+
+
+  const onNext = function(err) {
+
+    debug(runnable.title, 'onNext')
+
+    const fail = function() {
+      return next.call(this, err)
+    }
+    const noFail = function () {
+      return next.call(this)
+    }
+
+    if (err) {
+  
+      if (test._retries === -1) {
+        test._retries = getDefaultRetries()
+      }
+
+      if (isBeforeHook && test._currentRetry < test._retries) {
+        test.trueFn = test.fn
+        console.log('set fn from', test.fn)
+        test.fn = function() {
           throw err
         }
-        currentRetry++
-        shouldSkipTest = true
-        markPrevAsRetry()
+        return noFail()
       }
-      beforeEachFn.apply(this)
     }
 
-    _beforeEach(...args)
+    return fail()
   }
+  args[0] = onNext
 
-  window.it = (testName, testFn) => {
-    if (!testFn) {
-      _it(testName)
-      return
-    }
-    Array(MAX_RETRIES + 1)
-      .fill()
-      .map((x, i) => {
-        _it(`${testName} ${i ? `(retry ${i})` : ''}`, function() {
-          if (i === MAX_RETRIES) {
-            shouldSkipBefore = false
-          }
-          if (shouldSkipTest) {
-            shouldSkipTest = false
-            failHandler = normalFail
-            this.skip()
-          }
-          if (currentRetry < i) {
-            failHandler = removeSkipFail
-            this.skip()
-          }
 
-          failHandler = err => {
-            if (i === MAX_RETRIES) {
-              currentRetry = 0
-              throw err
-            }
-            currentRetry++
-            markPrevAsRetry()
-            this.skip()
-          }
 
-          testFn.apply(this)
-          shouldSkipBefore = true
-          currentRetry = 0
-        })
-      })
+  return _onRunnableRun.apply(this, [runnableRun, runnable, args])
+}
+
+addGlobalStyle(/*css*/`
+.command-state-retry {
+  color: orange
+}
+.command-state-retry .command-number {
+  /* display:none */
+}
+.command-state-retry .command-method span{
+  background-color: #FCB827;
+  color: white;
+  border-radius: 2px;
+  padding: 0 3px;
+  font-size: 11px;
+  display: inline-block;
+  height: 14px;
+  line-height: 16px;
+}
+
+
+`)
+
+function addGlobalStyle(css) {
+  var head, style
+  head = window.top.document.getElementsByTagName('head')[0]
+  if (!head) {
+    return
   }
-  window.it.skip = _it.skip
-  window.it.only = _it.only
-
-  Cypress.on('fail', (...args) => failHandler(...args))
-
-  const markPrevAsRetry = () => {
-    setTimeout(() => {
-      const el = [].slice
-        .call(
-          window.top.document.querySelectorAll(
-            '.runnable.runnable-pending > .runnable-wrapper .runnable-state'
-          )
-        )
-        .slice(-1)[0]
-      el.classList.remove('runnable-pending')
-      el.classList.add('runnable-retried')
-    }, 10)
-  }
-
-  addGlobalStyle(/*css*/`
-	.runnable.runnable-pending > .runnable-wrapper .runnable-state.runnable-retried {
-		color: orange
-	}
-	.runnable.runnable-skipped {
-		display:none
-	}
-	.runnable.runnable-pending > .runnable-wrapper .runnable-state.runnable-retried:before {
-		content: ""
-	}
-	`)
-
-  function addGlobalStyle(css) {
-    var head, style
-    head = window.top.document.getElementsByTagName('head')[0]
-    if (!head) {
-      return
-    }
-    style = window.top.document.createElement('style')
-    style.type = 'text/css'
-    style.innerHTML = css
-    head.appendChild(style)
-  }
+  style = window.top.document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = css
+  head.appendChild(style)
 }
