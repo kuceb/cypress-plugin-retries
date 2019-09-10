a520eada8bb05d0917dd7b188fe4707ed2b3de6b
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-09-09 22:19:59 -0400

    chore: setup CI, linting, tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,28 +3,28 @@
 Cypress.config('defaultCommandTimeout', 100)
 Cypress.env('RETRIES', 0)
 
-const _ = Cypress._
-
 describe('Cypress.currentTest', () => {
   let error
-  let curtest
-  try{
-    curtest = Cypress.currentTest
-  } catch(e) {
+
+  try {
+    Cypress.currentTest
+  } catch (e) {
     error = e
   }
+
   let lastId
 
-  it('throws error outside spec', function() {
+  it('throws error outside spec', function () {
     expect(error).ok.property('message').contains('currentTest')
     expect(this._runnable).eq(Cypress.currentTest)
-    curtest = Cypress.currentTest
+    Cypress.currentTest
     expect(lastId).eq(this._runnable.id)
   })
 
   beforeEach(() => {
     lastId = Cypress.currentTest.id
   })
+
   // Cypress.log({message:'set'})
   it('gets correct runnable', () => {
     expect(lastId).eq(Cypress.currentTest.id)
@@ -32,7 +32,7 @@ describe('Cypress.currentTest', () => {
 })
 
 describe('deeply nested', () => {
-  const pushHook = name => {
+  const pushHook = (name) => {
     // console.log(`%c${name}`, 'color:blue')
     expect(name).ok
     hooks.push(name)
@@ -40,16 +40,17 @@ describe('deeply nested', () => {
   let hooks = []
   let foo = 0
 
-  const failUntil = num => {
+  const failUntil = (num) => {
     if (num === true) {
       expect(false).ok
     }
+
     if (foo <= num) {
-      cy.get('failOn' + foo)
+      cy.get(`failOn${foo}`)
     }
   }
 
-  cy.on('fail', err => {
+  cy.on('fail', (err) => {
     hooks.push('FAIL')
     throw err
   })
@@ -59,10 +60,12 @@ describe('deeply nested', () => {
     pushHook('BE 0')
     foo++
   })
+
   describe('1', () => {
     before(() => {
       pushHook('B 1')
     })
+
     beforeEach(() => {
       pushHook('BE 1')
       failUntil(1)
@@ -73,12 +76,15 @@ describe('deeply nested', () => {
         pushHook('BE 2')
         failUntil(2)
       })
+
       beforeEach(() => {
         pushHook('BE 2 B')
       })
+
       beforeEach(() => {
         pushHook('BE 2 C')
       })
+
       it('T 2', () => {
         pushHook('T 2')
         failUntil(3)
@@ -104,8 +110,9 @@ describe('deeply nested', () => {
           'BE 2',
           'BE 2 B',
           'BE 2 C',
-          'T 2'
+          'T 2',
         ])
+
         hooks = []
       })
 
@@ -113,20 +120,25 @@ describe('deeply nested', () => {
         pushHook('T 2 B')
         failUntil(4)
       })
-      afterEach(function() {
+
+      afterEach(function () {
         pushHook('AE 2')
       })
     })
-    afterEach(function() {
+
+    afterEach(function () {
       pushHook('AE 1')
     })
-    afterEach(function() {
+
+    afterEach(function () {
       pushHook('AE 1 B')
     })
   })
+
   afterEach(() => {
     pushHook('AE 0')
   })
+
   after(() => {
     pushHook('AA 0')
     expect(hooks).deep.eq([
@@ -144,29 +156,32 @@ describe('deeply nested', () => {
       'AE 1',
       'AE 1 B',
       'AE 0',
-      'AA 0'
+      'AA 0',
     ])
   })
 })
 
 describe('async', () => {
-  it('pass using done', function(done) {
-    cy.on('fail', err => {
+  it('pass using done', function (done) {
+    cy.on('fail', (err) => {
       done()
     })
+
     expect(false).ok
   })
 })
 
-describe('fail 10 times', function() {
+describe('fail 10 times', function () {
   this.retries(10)
 
   let foo = 0
+
   beforeEach(() => {
     foo++
     expect(foo).gt(2)
   })
-  it('test', function() {
+
+  it('test', function () {
     expect(foo).gt(10)
     expect(this._runnable.currentRetry()).eq(10)
   })
@@ -180,9 +195,11 @@ describe('retries set via Cypress.currentTest.retries', () => {
     if (!Cypress.currentTest._currentRetry) {
       logs = []
     }
+
     cy.on('log:added', (attr, log) => {
       logs.push(attr)
     })
+
     Cypress.currentTest.retries(1)
   })
 
@@ -206,38 +223,46 @@ describe('after all hook', () => {
       it('fails but caught', () => {
         expect(true).ok
       })
+
       after(() => {
-        cy.on('fail', err => {
+        cy.on('fail', (err) => {
           didFail = true
           // Cypress.log({message: didFail})
           // throw err
         })
+
         expect(false).ok
       })
     })
+
     describe('verify prev fail', () => {
       it('did fail', () => {
         expect(didFail).ok
       })
     })
   })
+
   describe('after hook lets test fail', () => {
     let didFail = false
     let didRunAfterHook = false
+
     describe('should fail in after hook', () => {
       it('fails but caught', () => {
-        cy.on('fail', err => {
+        cy.on('fail', (err) => {
           didFail = true
           // Cypress.log({message: didFail})
           // throw err
         })
+
         expect(false).ok
       })
+
       after(() => {
         didRunAfterHook = true
         expect(true).ok
       })
     })
+
     describe('verify prev fail', () => {
       it('did fail', () => {
         expect(didFail).ok
--- src/index.d.ts
+++ src/index.d.ts
@@ -8,4 +8,3 @@
 					currentTest: Mocha.ISuiteCallbackContext
 			}
 	}
-
