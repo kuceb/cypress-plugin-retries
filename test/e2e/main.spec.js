c7e41ff2f713264f38031c935a4546181096d906
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-09-10 20:44:49 -0400

    chore: fix e2e tests in CI

--- test/e2e/main.spec.js
+++ test/e2e/main.spec.js
@@ -4,7 +4,7 @@ const path = require('path')
 describe('can test', () => {
   it('can fail with async hooks', async () => {
     await runTest({
-      spec: path.resolve(__dirname, '../../cypress/tests/e2e/after-hook.spec.js'),
+      spec: path.resolve(__dirname, '../../cypress/tests/e2e/after-hook-fail.spec.js'),
       statusCode: 1,
     })
   })
